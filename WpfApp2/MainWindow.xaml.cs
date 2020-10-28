1a6408fb5ac192890e3791eecbfdb1faf1544a3b
fuckshima <donmaicha1114@outlook.jp>
2020-10-28 22:39:34 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowVM();
+            Loaded += LoadedProc;
+        }
+        private async void LoadedProc(object sender, RoutedEventArgs e)
+        {
+            // WindowがActiveになるまで待つ
+            await Task.Run(() =>
+            {
+                do
+                {
+                    Thread.Sleep(100);
+                } while (!Application.Current.Dispatcher.Invoke(() => { return IsActive; }));
+            });
+            MainWindowVM vm = this.DataContext as MainWindowVM;
+            vm.StartTimer();
+        }
+    }
+}
---
+++ WpfApp2/ViewModels/MainWindowVM.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Timers;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// MainWindow用ViewModel
+    /// </summary>
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        private int percentage = 0;
+
+        public MainWindowVM()
+        {
+            t.Elapsed += T_Elapsed;
+        }
+        
+        private Timer t = new Timer(1);
+        public void StartTimer()
+        {
+            t.Start();
+        }
+
+        private void T_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (Percentage < 100)
+            {
+                Percentage += 1;
+
+            }
+            else
+            {
+                Percentage = 0;
+            }
+        }
+
+
+        public int Percentage
+        {
+            get
+            {
+                return this.percentage;
+            }
+            set
+            {
+                this.percentage = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
